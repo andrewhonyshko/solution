60d7cbbc0606346650eafa488493983ef94cb05a
ГОНИШКО Андрій Петрович <angol@sc.court.local>
2021-06-07 10:38:49 +0300

    Decorator

--- Solution.java
+++ Solution.java
@@ -1,15 +1,6 @@
 package solution;
 
-import java.awt.*;
-import java.io.*;
-<<<<<<< HEAD
-import java.nio.Buffer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
+
 
 /* 
 И еще один адаптер
@@ -23,16 +14,3 @@ public class Solution {
         System.out.println(developer.makeJob());
     }
 }
-=======
-import java.util.*;
-
-public class Solution {
-    public static void main(String[] args) throws IOException
-    {
-        USB usb=new CardReader(new MemoryCard());
-        usb.connectWithUsbCable();
-        usb=new Cable(new Telephone());
-        usb.connectWithUsbCable();
-    }
-}
->>>>>>> 8be50d1ece75abd1979a95b65495ff50f55da243
